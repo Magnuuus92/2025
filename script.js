2fbf2b1b5f3ee0a33e42359f929716da38c0d8dd
Magnuuus92 <Magnus92.kodehode@gmail.com>
2025-09-15 09:56:10 +0200

    js start kode

---
+++ script.js
@@ -0,0 +1,12 @@
+//Dette er en kodekommentar
+//bruke consol.log metoden for å skrive ut meldingen hei.
+
+console.log("hei");
+
+//flere linjer med
+// kommentar
+// en text i kodespråk er en "string"
+
+console.log("hallo, hva heter du");
+
+console.log();
