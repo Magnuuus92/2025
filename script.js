8a8ea7cc28d3b307960fd59de417af016ba29dfb
Magnuuus92 <Magnus92.kodehode@gmail.com>
2025-09-15 13:54:20 +0200

    flere buttons

--- script.js
+++ script.js
@@ -29,4 +29,24 @@ function makeitred() {
 }
 buttonElementRed.addEventListener("click", makeitred);
 
+let buttonElementRevert = document.querySelector("#button-revert");
+//2. to see if it worked we can then console it.
+console.log(buttonElementRevert);
+//3.Try to use addeventlistener...
+function makeitrevert() {
+  //function shall change the color of the h1 element to green
+  //1. grab h1 elemnt:
+  let h1Element = document.querySelector("h1");
+  //console.log(h1Element);
+  h1Element.style = "color: wheat;";
+}
+buttonElementRevert.addEventListener("click", makeitrevert);
+
+let buttonElementBlack = document.querySelector("#button-black");
+console.log(buttonElementBlack);
+function makeitblack() {
+  let h1Element = document.querySelector("h1");
+  h1Element.style = "color: black;";
+}
+buttonElementBlack.addEventListener("click", makeitblack);
 //II.handle the red color button:
