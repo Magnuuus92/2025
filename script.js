edebbc17bdf9977db52c01b0cb80a39176d5bf20
Magnuuus92 <Magnus92.kodehode@gmail.com>
2025-09-16 11:20:23 +0200

    button counters

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let h1Element = document.querySelector("h1");
 let buttonElementGreen = document.querySelector("#button-green");
 let buttonElementRed = document.querySelector("#button-red");
@@ -36,3 +37,32 @@ function makeitbrown() {
 buttonElementBrown.addEventListener("click", makeitbrown);
 
 //II.handle the red color button:
+*/
+//get the current number element
+let currentNumberEl = document.querySelector("#current-number");
+let congratulationsEl = document.querySelector("#congratulations");
+console.log(currentNumberEl);
+console.log(congratulationsEl);
+// get the button elements and store them for ease of  access
+let buttonPlus = document.querySelector("#button-plus");
+let buttonMinus = document.querySelector("#button-minus");
+let number = 0;
+//increase logic: Function increases the variable called number with +1
+//and then displays that number in the textcontent property of and element called currentNumberEl
+function increase() {
+  number = number + 1;
+  currentNumberEl.textContent = number;
+  console.log("Number is: ", number);
+}
+function decrease() {
+  number = number - 1;
+  currentNumberEl.textContent = number;
+  console.log("the number is: ", number);
+}
+buttonPlus.addEventListener("click", increase);
+buttonMinus.addEventListener("click", decrease);
+//set number to a value with the assignment operater (=)
+//check if number is equal to with the comparison operator (==)
+if (number == 5) {
+  congratulationsEl.textContent = "great news, the number is 5!";
+}
